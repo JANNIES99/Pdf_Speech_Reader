66d740b767de73a1342ce3f6dae91d57b92dc7ee
JANNIES <janniesvarghese@gmail.com>
2025-04-10 21:07:47 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  FlutterTts flutterTts = FlutterTts();
+  Map? currentVoice;
+  @override
+  void initState() {
+    super.initState();
+    initTTS();
+  }
+
+  void initTTS() {
+    flutterTts.getVoices.then((data) {
+      try {
+        List<Map> voices = List<Map>.from(data);
+        print(voices);
+        setState(() {});
+      } catch (e) {
+        print(e);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
