35d16b18dfc0b0540ede20e4bd3e2b0d527eab6d
JANNIES <janniesvarghese@gmail.com>
2025-04-10 22:25:23 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:pdfaudioreader/consts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   FlutterTts flutterTts = FlutterTts();
+  int? currentWordStart;
+  int? currentWordEnd;
+  List<Map> voices = [];
   Map? currentVoice;
   @override
   void initState() {
@@ -22,13 +26,20 @@ class _MyAppState extends State<MyApp> {
   }
 
   void initTTS() {
+    flutterTts.setProgressHandler((text, start, end, word) {
+      setState(() {
+        currentWordStart = start;
+        currentWordEnd = end;
+      });
+    });
     flutterTts.getVoices.then((data) {
       try {
-        List<Map> voices = List<Map>.from(data);
+        voices = List<Map>.from(data);
         voices = voices.where((voice) => voice["name"].contains("en")).toList();
         print(voices);
         setState(() {
           currentVoice = voices.first;
+          setVoice(currentVoice!);
         });
       } catch (e) {
         print(e);
@@ -36,8 +47,71 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void setVoice(Map voice) {
+    flutterTts.setVoice({"name": voice["name"], "locale": voice["locale"]});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              speakerSelector(),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(text: TTS_Input.substring(0, currentWordStart)),
+                    if (currentWordStart != null)
+                      TextSpan(
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          backgroundColor: Colors.deepPurple,
+                        ),
+                        text: TTS_Input.substring(
+                          currentWordStart!,
+                          currentWordEnd,
+                        ),
+                      ),
+                    if (currentWordEnd != null)
+                      TextSpan(text: TTS_Input.substring(currentWordEnd!)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            flutterTts.speak(TTS_Input);
+          },
+          child: Icon(Icons.speaker),
+        ),
+      ),
+    );
+  }
+
+  Widget speakerSelector() {
+    return DropdownButton(
+      value: currentVoice,
+      items:
+          voices
+              .map(
+                (voice) =>
+                    DropdownMenuItem(value: voice, child: Text(voice["name"])),
+              )
+              .toList(),
+      onChanged: (voice) {
+        setState(() {
+          currentVoice = voice;
+          setVoice(currentVoice!);
+        });
+      },
+    );
   }
 }
